48c4782b56ff7ed7061e4a0add9ddfca4b250095
Christopher <chrisgregorian@gmail.com>
2018-03-07 23:23:14 +0100

    mehr php tests

--- action.php
+++ action.php
@@ -9,6 +9,13 @@ Your email address is: <?php echo $_POST["email"]; ?>
 
 <?php
 	$my_file = '/var/www/heiraten/file.txt';
+	
+	if (file_exists($my_file)) {
+    echo "The file $filename exists";
+	} else {
+    echo "The file $filename does not exist";
+	}
+	
 	$handle = fopen($my_file, 'a') or die('Cannot open file:  '.$my_file);
 	$data = $_POST["name"] . $_POST["email"];
 	fwrite($handle, $data);
